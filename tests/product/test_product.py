6cf3167ff7aee69d7ef3abe263710b0e571da462
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-04 15:01:54 -0300

    test: for constructor method of Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,35 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = {
+        "id": 95432,
+        "nome_do_produto": "Produto ficticio",
+        "nome_da_empresa": "Empresa ficticia",
+        "data_de_fabricacao": "10/08/2019",
+        "data_de_validade": "25/08/2024",
+        "numero_de_serie": "nsf021585",
+        "instrucoes_de_armazenamento": "local seco e arejado",
+    }
+
+    produto_ficticio = Product(
+        product["id"],
+        product["nome_do_produto"],
+        product["nome_da_empresa"],
+        product["data_de_fabricacao"],
+        product["data_de_validade"],
+        product["numero_de_serie"],
+        product["instrucoes_de_armazenamento"],
+    )
+
+    assert produto_ficticio.id == product["id"]
+    assert produto_ficticio.nome_do_produto == product["nome_do_produto"]
+    assert produto_ficticio.nome_da_empresa == product["nome_da_empresa"]
+    assert produto_ficticio.data_de_fabricacao == product["data_de_fabricacao"]
+    assert produto_ficticio.data_de_validade == product["data_de_validade"]
+    assert produto_ficticio.numero_de_serie == product["numero_de_serie"]
+    # indented in autosave by flake8
+    assert (
+        produto_ficticio.instrucoes_de_armazenamento
+        == product["instrucoes_de_armazenamento"]
+    )
