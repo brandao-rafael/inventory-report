9634551aa2e020aa2bd40d77390123c2f8d9c2c0
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-05 10:33:34 -0300

    feat: method to read json file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,11 +1,12 @@
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+import json
 import csv
 
 
 class Inventory:
     @staticmethod
-    def read(path):
+    def read_csv(path):
         with open(path, mode="r", encoding="utf8") as file:
             file_reader = csv.reader(file)
             header, *data = file_reader
@@ -19,8 +20,28 @@ class Inventory:
         return product_list
 
     @staticmethod
+    def read_json(path):
+        with open(path, mode="r", encoding="utf8") as file:
+            contents = file.read()
+            data = json.loads(contents)
+            header = data[0].keys()
+        product_list = []
+
+        for row in data:
+            product = {}
+            for column in header:
+                product[column] = row.get(column)
+            product_list.append(product)
+        return product_list
+
+    @staticmethod
     def import_data(path: str, report_type: str):
-        products = Inventory.read(path)
+        products = []
+        if path.endswith(".json"):
+            products = Inventory.read_json(path)
+        else:
+            products = Inventory.read_csv(path)
+
         if report_type == "simples":
             simple = SimpleReport.generate(products)
             return simple
