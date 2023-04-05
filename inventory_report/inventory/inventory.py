3014c4b2131a032f40210fac79eb9c9fd701b139
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-05 11:05:39 -0300

    refactor: removing unselles logics for read_json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -25,15 +25,7 @@ class Inventory:
         with open(path, mode="r", encoding="utf8") as file:
             contents = file.read()
             data = json.loads(contents)
-            header = data[0].keys()
-        product_list = []
-
-        for row in data:
-            product = {}
-            for column in header:
-                product[column] = row.get(column)
-            product_list.append(product)
-        return product_list
+        return data
 
     @staticmethod
     def read_xml(path):
